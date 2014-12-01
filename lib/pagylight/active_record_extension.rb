83327d1db6ccd0cf6640fd5f1eec3e98ed43781c
Andrew Yasinishyn <yasinishyn.a.n@gmai.com>
2014-12-01 09:39:17 +0200

    pages array

--- lib/pagylight/active_record_extension.rb
+++ lib/pagylight/active_record_extension.rb
@@ -6,12 +6,27 @@ module Pagylight
     # where firt parameter is page, and second is record per page
     #
     def pagylight page, records
+      @_page  = page
       @_pages = count / records
-      offset(page*records).limit(records)
+      offset(page*records-records).limit(records)
     end
 
+    # 
+    # If we will have 10 records per page, and total of 
+    # 500 pages, the output will loock like this:
+    # 
+    # [1,2,  3,  4,  5,  6,  7,  8,  8,  500]
+    # [1,4,  5,  6,  7,  8,  9,  10, 11, 500]
+    # [1,492,493,494,495,496,497,498,499,500]
+    # 
+    # You will always get first and last page
+    #
     def pages
-      [*1..@_pages]
+      return [*1..@_pages]                 if @_pages <= 10
+      return [*1..8] + [@_pages]           if  @_page <= 5
+      return [1] + [*(@_pages-7)..@_pages] if [*(@_pages-7)..@_pages].include? @_page
+
+      [1]+[*(@_page-3)..(@_page+3)]+[@_pages]
     end
   end
 end
